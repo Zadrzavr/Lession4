defefc6a2740617b0cceae74f176cd6e21b46661
Zadrzavr <83001982+Zadrzavr@users.noreply.github.com>
2021-12-28 01:55:19 +0700

    Update Main.java

--- lession4/Main.java
+++ lession4/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        String[] words = {"Hello", "Car", "Witcher", "Cat", "Dog", "God","Angels", "USA", "Box", "Riven"};
+        String[] words = {"Hello", "Car", "Witcher", "Cat", "Dog", "God","Angels", "USA", "Box", "Riven", "Car"};
 
         HashMap<String, Integer> Words = new HashMap<>();
 
